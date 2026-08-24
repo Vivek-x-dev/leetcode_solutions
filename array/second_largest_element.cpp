99f5744c02dd116bc7ec7040f97a1a1aa90c02d5
VIVEK KUMAR <vivekdaviet@gmail.com>
2026-08-24 23:13:54 +0530

    ap 1,2,3

--- array/second_largest_element.cpp
+++ array/second_largest_element.cpp
@@ -4,11 +4,14 @@
 
  Note: The second largest element should not be equal to the largest element.
 */
+
+// <--------------approach 1--------------->
+
 class Solution {
   public:
     int getSecondLargest(vector<int> &arr) {
         int n = arr.size(); 
-        sort (arr.begin(),arr.end());
+        sort (arr.begin(),arr.end());              /// O(nlogn)
         for(int i= n - 2 ; i >=  0 ; i-- ){
             if (arr[i]!=arr[n-1]){
                 return arr[i];
@@ -18,5 +21,54 @@ class Solution {
         return -1;
         
     }
+};      //brute force 
+
+// <--------------approach 2---------------->
+
+class Solution {
+  public:
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+
+    int largest = -1, secondLargest = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];                               //O(2n)= O(n)
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
 };
-//brute force 
+
+// <--------------approach 3---------------->
+
+
+class Solution {
+  public:
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+
+    int largest = -1, secondLargest = -1;
+
+    
+    for (int i = 0; i < n; i++) {                                   ///O(n)
+
+       
+        if(arr[i] > largest) {
+        	secondLargest = largest;
+            largest = arr[i];
+        }
+      
+      	else if(arr[i] < largest && arr[i] > secondLargest) {
+        	secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+};
